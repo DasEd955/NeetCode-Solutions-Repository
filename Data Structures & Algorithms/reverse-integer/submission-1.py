ab899875e77065abf2b77c7c52d33aa73abd633f
DasEd955 <info@tunerdesign.org>
2026-07-03 22:03:30 -0400

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        str_x = str(x)
+        list_x = list(str_x)
+        if list_x[0] == '-':
+            list_x.remove('-')
+            list_x.reverse()
+            result = str()
+            for char in list_x:
+                result += char
+            result =  -1 * int(result)
+            if result < -2**31 or result > 2**31 - 1:
+                return 0
+            return result
+        else:
+            list_x.reverse()
+            result = str()
+            for char in list_x:
+                result += char
+            result = int(result)
+            if result < -2**31 or result > 2**31 - 1:
+                return 0
+            return result
