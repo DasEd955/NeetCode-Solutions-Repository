b954738e5e19c7e59d52ce1a773b94bbd5020664
DasEd955 <info@tunerdesign.org>
2026-07-06 17:03:08 -0400

    Remove: plus-one - submission-0

--- Data Structures & Algorithms/plus-one/submission-0.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        result = 0
-        for digit in digits:
-            result = (result * 10) + digit
-        return list(str(result + 1))
