6e340c506628bbd9d7eba9d5440932a332380815
DasEd955 <info@tunerdesign.org>
2026-07-08 12:55:29 -0400

    Remove: rotate-matrix - submission-2

--- Data Structures & Algorithms/rotate-matrix/submission-2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        ROWS, COLS = len(matrix), len(matrix[0])
-
-        matrix.reverse()
-        
-        for r in range(ROWS):
-            for c in range(r, COLS):
-                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
